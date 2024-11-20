3618a10e64b17f8d338dbda99d07346af5f0f050
Sahil Saini <sahils@valuefy.com>
2024-11-20 13:05:54 +0530

    Array Implementation Succes in C

--- 1. Array/array.c
+++ 1. Array/array.c
@@ -1,137 +1,145 @@
-#include<stdio.h>
-#include<stdlib.h>
+#include <stdio.h>
+#include <stdlib.h>
 
-int* ptr, arrayLenght = 0;
+int *ptr = NULL, arrayLength = 0;
 
-void generateArray(){
-    if (arrayLenght == 0){
-        arrayLenght++;
-        ptr = (int*)calloc(arrayLenght, sizeof(int));
+void generateArray(int len) {
+    if (len > 0) {
+        arrayLength = len;
+        ptr = (int *)realloc(ptr, arrayLength * sizeof(int));
         if (ptr == NULL) {
-            printf("memory cannot be allocated");
-        }
-    }
-    else {
-        arrayLenght++;
-        ptr = (int*)realloc(ptr, arrayLenght * sizeof(int));
-        if (ptr == NULL) {
-            printf("memory cannot be allocated");
+            printf("Memory allocation failed.\n");
+            exit(1); // Exit the program on allocation failure
         }
     }
 }
 
-void print(){ // Show Data
-    if (arrayLenght == 0){
+void print() { // Show Data
+    if (arrayLength == 0) {
         printf("\n~~ Array is Empty! ~~\n");
     }
     else {
-        printf("\nData:- %d",ptr[0]);
-        for (int i = 1; i < arrayLenght; i++){
+        printf("\nData:- %d", ptr[0]);
+        for (int i = 1; i < arrayLength; i++) {
             printf(", %d", ptr[i]);
         }
+        printf("\n");
     }
 }
 
-void reversePrint(){ // Show in Reverse
-    if (arrayLenght == 0){
-        printf("\nArray is Empty!");
+void reversePrint() { // Show in Reverse
+    if (arrayLength == 0) {
+        printf("\n~~ Array is Empty! ~~\n");
     }
     else {
-        printf("\nData:- %d",ptr[arrayLenght - 1]);
-        for (int i = arrayLenght - 1; i > 0; i--){
+        printf("\nData(Reverse):- %d", ptr[arrayLength - 1]);
+        for (int i = arrayLength - 1; i > 0; i--) {
             printf(", %d", ptr[i - 1]);
         }
+        printf("\n");
     }
 }
 
-int length(){ // Lenght of Data
-    return arrayLenght;
+int length() { // Length of Data
+    printf("\nData Length: %d\n", arrayLength);
+    return arrayLength;
 }
 
-void push(){ // Insert At Last
-    generateArray();
+void push() { // Insert At Last
+    generateArray(arrayLength + 1);
     int val;
     printf("\nEnter Value:- ");
     scanf("%d", &val);
-    ptr[arrayLenght - 1] = val;
-    printf("Insertion success Value:- %d\n", val);
+    ptr[arrayLength - 1] = val;
+    printf("Insertion Successfull. Value:- %d\n", val);
 }
 
-void pop(){ // Remove From Last
-
+void pop() { // Remove From Last
+    if (arrayLength == 0) {
+        printf("\n~~ Array is Empty! Cannot Remove ~~\n");
+        return;
+    }
+    printf("\nRemoved Value: %d\n", ptr[arrayLength - 1]);
+    generateArray(arrayLength - 1);
 }
 
-void insertInPlace(){ // Insert at Specific Position
+void insertInPlace() { // Insert at Specific Position
     int val, pos;
     printf("\nEnter Value:- ");
     scanf("%d", &val);
     printf("Enter Position Number to Insert(Greater than 0):- ");
     scanf("%d", &pos);
-    if(pos < 0){
-        printf("Invalid Position:- %d", pos);
+
+    if (pos <= 0 || pos > arrayLength + 1) {
+        printf("Invalid Position: %d\n", pos);
         return;
     }
 
-    if(pos <= arrayLenght){
-        generateArray();
-        int dum = arrayLenght;
-        while (dum > pos){
-            ptr[arrayLenght - 1] = ptr[arrayLenght - 2];
-            dum--;
-        }
-        ptr[pos - 1] = val;
-        printf("Insertion Successfull at %d, Value:- %d", pos, val);
-    } 
-    else if(pos > arrayLenght){
+    generateArray(arrayLength + 1);
 
+    for (int i = arrayLength - 1; i >= pos - 1; i--) {
+        ptr[i] = ptr[i - 1]; // Shift elements to the right
     }
-    ptr[arrayLenght] = val;
-    printf("\nInsertion success Value:- %d", val);
+    ptr[pos - 1] = val;
+    printf("Insertion Successful at Position %d. Value: %d\n", pos, val);
 }
 
-void deleteInPlace(){ // Remove From Specific Position
+void deleteInPlace() { // Remove From Specific Position
+    int pos;
+    printf("\nEnter Position to Remove(Greater than 0):- ");
+    scanf("%d", &pos);
 
+    if (pos <= 0 || pos > arrayLength) {
+        printf("Invalid Position: %d\n", pos);
+        return;
+    }
+
+    printf("Removed Value: %d\n", ptr[pos - 1]);
+
+    for (int i = pos - 1; i < arrayLength - 1; i++) {
+        ptr[i] = ptr[i + 1]; // Shift elements to the left
+    }
+
+    generateArray(arrayLength - 1);
 }
 
-int main(){
-    int i = 0, choice;
+int main() {
+    int choice;
     printf("Hello! Here You Can Perform Following Array Operation!");
-    while(i == 0){
+    while (1) {
         printf("\n1. Show Data\n2. Show in Reverse\n3. Insert At Last\n4. Insert at Specific Position\n5. Remove From Last\n6. Remove From Specific Position\n7. Lenght of Data\n8. Exit\nEnter Your Choice:- ");
         scanf("%d", &choice);
 
-        switch(choice) {
-            case 1:
-                print();
-                break;
-            case 2:
-                reversePrint();
-                break;
-            case 3:
-                push();
-                break;
-            case 4:
-                insertInPlace();
-                break;
-            case 5:
-                pop();
-                break;
-            case 6:
-                deleteInPlace();
-                break;
-            case 7:
-                length();
-                break;
-            case 8:
-                free(ptr);
-                printf("Thanks For Using! Have a Great Day!");
-                i = 1;
-                break;
-            default:
-                printf("Please Enter a Vaild Input!\n");
-                break;
+        switch (choice) {
+        case 1:
+            print();
+            break;
+        case 2:
+            reversePrint();
+            break;
+        case 3:
+            push();
+            break;
+        case 4:
+            insertInPlace();
+            break;
+        case 5:
+            pop();
+            break;
+        case 6:
+            deleteInPlace();
+            break;
+        case 7:
+            length();
+            break;
+        case 8:
+            free(ptr);
+            printf("Thanks For Using! Have a Great Day!");
+            return 0;
+        default:
+            printf("Please Enter a Vaild Input!\n");
+            break;
         }
-        
     }
     return 0;
 }
