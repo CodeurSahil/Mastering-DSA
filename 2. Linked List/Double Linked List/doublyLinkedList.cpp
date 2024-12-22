f81d14f0fc241ee6d5be111554d0794acb350ab1
Sahil Saini <sahils@valuefy.com>
2024-12-22 12:18:25 +0530

    Implemented Linked List in C++

--- 2. Linked List/Double Linked List/doublyLinkedList.cpp
+++ 2. Linked List/Double Linked List/doublyLinkedList.cpp
@@ -1,6 +1,6 @@
-
-#include <stdio.h>
+#include <iostream>
 #include <stdlib.h>
+using namespace std;
 
 
 struct node {  
@@ -15,48 +15,49 @@ int listLength = 0;
 struct node * createLinkedListNode() {
     struct node *temp = (struct node *)malloc(sizeof(struct node));
     if (temp == NULL) {
-        printf("Memory allocation failed.\n");
+        cout << "Memory allocation failed.\n";
         exit(1); // Exit the program on allocation failure
     }
     temp -> next = NULL;
     temp -> prev = NULL;
-    listLength++;
+    listLength ++;
     return temp;
 }
 
 void print(int reverse) { // Show Data
     if (listLength == 0) {
-        printf("\n~~ Linked List is Empty! ~~\n");
+        cout << "\n~~ Linked List is Empty! ~~\n";
         return;
     }
+
     if (reverse) {
-        printf("\nLinked List(Reverse): %d -> ", tail -> data);
+        cout << "\nLinked List(Reverse): " << tail -> data << " -> ";
         struct node *current = tail;
         while (current -> prev != NULL) {
             current = current->prev;
-            printf("%d -> ", current->data);
+            cout << current->data <<" -> ";
         }
-        printf("NULL\n");
+        cout << "NULL\n";
     }
     else {
-        printf("\nLinked List: %d -> ", head -> data);
+        cout << "\nLinked List: " << head -> data << " -> ";
         struct node *current = head;
         while (current -> next != NULL) {
             current = current -> next;
-            printf("%d -> ", current->data);
+            cout << current->data << " -> ";
         }
-        printf("NULL\n");
+        cout << "NULL\n";
     }
 }
 
 void insert(int val, int pos) { // Insert
     if (pos <= 0 || pos > listLength + 1) {
-        printf("\n~~Invalid Position: %d~~\n", pos);
+        cout << "\n~~Invalid Position: " << pos << "~~\n";
         return;
     }
 
     struct node *newNode = createLinkedListNode();
-    newNode -> data = val;
+    newNode->data = val;
 
     if (pos == 1) {
         if(head){
@@ -93,17 +94,18 @@ void insert(int val, int pos) { // Insert
         dum -> next -> prev = newNode;
         dum -> next = newNode;
     }
-    printf("Insertion Successful at Position %d. Value: %d\n", pos, val);
+    cout << "Insertion Successful at Position:- " << pos << ". Value:- " << val << "\n";
 }
 
-void delete(int pos) { // Remove 
+void deleteNode(int pos) { // Remove 
     if (pos <= 0 || pos > listLength) {
-        printf("\n~~Invalid Position: %d~~\n", pos);
+        cout << "\n~~Invalid Position: " << pos << "~~\n";
         return;
     }
 
     int deletedValue;
     struct node *temp = head, *deletedNode;
+
     if(pos == 1){
         head = head -> next;
         head -> prev = NULL;
@@ -131,18 +133,18 @@ void delete(int pos) { // Remove
     }
 
     listLength--;
-    printf("\nRemoved Value: %d\n", deletedValue);
+    cout << "\nRemoved Value: " << deletedValue << "\n";
 }
 
 int main() {
     int choice;
 
-    printf("Hello! Here You Can Perform Following Linked List Operations!\n");
+    cout << "Hello! Here You Can Perform Following Doubly Linked List Operations!\n";
     while (1) {
-        printf("\n1. Show Data\n2. Show Data(Reverse)\n3. Insert at First\n4. Insert at Last\n5. Insert at Specific Position\n");
-        printf("6. Remove from First\n7. Remove from Last\n8. Remove from Specific Position\n");
-        printf("9. Length of Data\n10. Exit\nEnter Your Choice: ");
-        scanf("%d", &choice);
+        cout << "\n1. Show Data\n2. Show Data(Reverse)\n3. Insert at First\n4. Insert at Last\n5. Insert at Specific Position\n";
+        cout << "6. Remove from First\n7. Remove from Last\n8. Remove from Specific Position\n";
+        cout << "9. Length of Data\n10. Exit\nEnter Your Choice: ";
+        cin >> choice;
 
         int val, pos;
         switch (choice) {
@@ -153,53 +155,53 @@ int main() {
             print(1);
             break;
         case 3:
-            printf("\nEnter Value: ");
-            scanf("%d", &val);
+            cout << "\nEnter Value: ";
+            cin >> val;
             insert(val, 1);
             break;
         case 4:
-            printf("\nEnter Value: ");
-            scanf("%d", &val);
+            cout << "\nEnter Value: ";
+            cin  >> val;
             insert(val, listLength + 1);
             break;
         case 5:
-            printf("\nEnter Value: ");
-            scanf("%d", &val);
-            printf("Enter Position: ");
-            scanf("%d", &pos);
+            cout << "\nEnter Value: ";
+            cin >> val;
+            cout << "Enter Position: ";
+            cin >> pos;
             insert(val, pos);
             break;
         case 6:
             if (listLength == 0) {
-                printf("\n~~ Linked List is Empty! Cannot Remove ~~\n");
+                cout << "\n~~ Linked List is Empty! Cannot Remove ~~\n";
             } else {
-                delete(1);
+                deleteNode(1);
             }
             break;
         case 7:
             if (listLength == 0) {
-                printf("\n~~ Linked List is Empty! Cannot Remove ~~\n");
+                cout << "\n~~ Linked List is Empty! Cannot Remove ~~\n";
             } else {
-                delete(listLength);
+                deleteNode(listLength);
             }
             break;
         case 8:
             if (listLength == 0) {
-                printf("\n~~ Linked List is Empty! Cannot Remove ~~\n");
+                cout << "\n~~ Linked List is Empty! Cannot Remove ~~\n";
             } else {
-                printf("\nEnter Position: ");
-                scanf("%d", &pos);
-                delete(pos);
+                cout << "\nEnter Position: ";
+                cin >> pos;
+                deleteNode(pos);
             }
             break;
         case 9:
-            printf("\nData Length: %d\n", listLength);
+            cout << "\nList Length: " << listLength << "\n";
             break;
         case 10:
-            printf("\n~~Thanks for using! Have a great day!~~");
+            cout << "\n~~Thanks for using! Have a great day!~~";
             return 0;
         default:
-            printf("\n~~Please Enter a Valid Input!~~\n");
+            cout << "\n~~Please Enter a Valid Input!~~\n";
         }
     }
 
